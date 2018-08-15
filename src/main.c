efabd1ab0ea7750c25979e0799e210dd5e1fab39
Eugen Brusilovskyy <e.brusilovskyy@dsfishlabs.com>
2018-08-15 17:02:44 +0200

    Added getopt() for windows

--- src/main.c
+++ src/main.c
@@ -1,6 +1,45 @@
 #include "fbx_utils.h"
 
-void main(void)
+#include <string.h>
+
+#define AppMain main
+
+#ifndef _MSC_VER
+#include <getopt.h>
+#else // _MSC_VER
+#include "xgetopt.h"
+#endif // _MSC_VER
+
+struct CommandLine_
 {
-	FbxLoadScene();
+	const char* InFile;
+};
+
+typedef struct CommandLine_ CommandLine;
+
+void AppParseCommandLine(int argc, char** argv, CommandLine* cmd)
+{
+	int option = 0;
+	while ((option = getopt(argc, argv, "i:o:")) != -1)
+	{
+		switch (option)
+		{
+		case 'i':
+			cmd->InFile = optarg;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void AppMain(int argc, char** argv)
+{
+	CommandLine cmd;
+	memset(&cmd, 0, sizeof(cmd));
+	AppParseCommandLine(argc, argv, &cmd);
+	if (cmd.InFile)
+	{
+		FbxLoadScene();
+	}
 }
---
+++ src/xgetopt.h
@@ -0,0 +1,31 @@
+// XGetopt.h  Version 1.2
+//
+// Author:  Hans Dietrich
+//          hdietrich2@hotmail.com
+//
+// This software is released into the public domain.
+// You are free to use it in any way you like.
+//
+// This software is provided "as is" with no expressed
+// or implied warranty.  I accept no liability for any
+// damage or loss of business that this software may cause.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef XGETOPT_H
+#define XGETOPT_H
+
+#ifdef __cplusplus
+#define C_API extern "C"
+#else // __cplusplus
+#define C_API extern
+#endif // __cplusplus
+
+#include <tchar.h>
+
+C_API int optind, opterr;
+C_API TCHAR *optarg;
+
+C_API int getopt(int argc, TCHAR *argv[], TCHAR *optstring);
+
+#endif //XGETOPT_H
