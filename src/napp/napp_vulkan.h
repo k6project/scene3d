d2d7c3298bb758fb3585caac2ea391dc0c093ddf
Eugen Brusilovskyy <eugen.brusilowski@gmail.com>
2018-09-19 08:48:27 +0200

    Initializing Vulkan library on Mac

--- src/napp/napp_vulkan.h
+++ src/napp/napp_vulkan.h
@@ -8,6 +8,9 @@
 #define VULKAN_API_DEVICE(f) extern PFN_vk ## f ## vk ## f ;
 #include "napp_vkdevice.h"
 
+#define VKTEST(call) do { VkResult VKRESULT = call ; TEST(VKRESULT==VK_SUCCESS);}  while (0)
+#define VKINIT(obj,tname) do { memset(&obj, 0, sizeof(obj));(obj).sType = tname; } while (0)
+
 NAPP_API void NAppVkInit();
 
 NAPP_API VkDevice NAppVkCreateDevice(int phDevice, const VkDeviceCreateInfo* info);
