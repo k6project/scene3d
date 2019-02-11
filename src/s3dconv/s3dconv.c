a0244f18a88de9516fd6b7f56001d0a4483ee560
Eugen Brusilovskyy <eugen.brusilowski@gmail.com>
2019-02-11 09:21:09 +0100

    Added function to open and close file

--- src/s3dconv/s3dconv.c
+++ src/s3dconv/s3dconv.c
@@ -1,3 +1,7 @@
+#include <vecmath.h>
+
+#include <stdio.h>
+
 #ifdef _WIN32
     #ifdef _DEBUG
 	    #undef _DEBUG
@@ -24,6 +28,18 @@
 
 #define NumVertexComponents 6
 #define PyModuleEntryPoint  PyInit_s3dconv
+
+typedef struct
+{
+    uint32_t index;
+    Vec3f pos, norm;
+    
+} PyVertexEntry;
+
+typedef struct
+{
+    FILE* file;
+} PyModuleState;
 
 static PyObject* PyAbout(PyObject *self, PyObject *args)
 {
@@ -71,18 +87,48 @@ static PyObject* PyAddVertex(PyObject *self, PyObject *args)
     return PyLong_FromLong(0);
 }
 
+static PyObject* PyBegin(PyObject *self, PyObject *args)
+{
+    const char* fileName = PyUnicode_AsUTF8(args);
+    PyModuleState* state = (PyModuleState*)PyModule_GetState(self);
+    if (fileName && state && !state->file)
+    {
+        state->file = fopen(fileName, "wb");
+        struct { char magick[3], type; } header = { {'S', '3', 'D'}, 'G' };
+        fwrite(&header, sizeof(header), 1, state->file);
+    }
+    return Py_None;
+}
+
+static PyObject* PyEnd(PyObject *self, PyObject *args)
+{
+    PyModuleState* state = (PyModuleState*)PyModule_GetState(self);
+    if (state && state->file)
+    {
+        fclose(state->file);
+    }
+    return Py_None;
+}
+
 PyMODINIT_FUNC PyModuleEntryPoint(void)
 {
 	static PyMethodDef methods[] =
 	{
 		{ "about",  PyAbout, METH_NOARGS, "About this module." },
 		{ "header",  PyGetHeaderString, METH_NOARGS, "Get header string." },
-        { "add_vertex",  PyAddVertex, METH_O, "Add new vertex/normal." },
+        
+        { "begin",  PyBegin, METH_O, "Create new export file" },
+        { "add_vertex",  PyAddVertex, METH_O, "Find/add new vertex entry" },
+        { "end", PyEnd, METH_NOARGS, "Close current file" },
+        
 		{ NULL, NULL, 0, NULL }
 	};
 	static struct PyModuleDef module =
 	{
-		PyModuleDef_HEAD_INIT, "s3dconv", /*doc*/NULL, /*state*/-1, methods
+		PyModuleDef_HEAD_INIT, "s3dconv", NULL, sizeof(PyModuleState), methods
 	};
-	return PyModule_Create(&module);
+    PyObject* mod = PyModule_Create(&module);
+    PyModuleState* state = (PyModuleState*)PyModule_GetState(mod);
+    memset(state, 0, sizeof(PyModuleState));
+	return mod;
 }
